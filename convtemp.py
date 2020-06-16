6651b2084c5dc0d77da05f198856b1a2d973067d
Luis Fernando <luisfgiglio@gmail.com>
2020-06-16 16:48:58 -0300

    atualizado

--- convtemp.py
+++ convtemp.py
@@ -1,116 +1,155 @@
 from prettytable import PrettyTable
 
-#definição de itens e listas.
+
 def espaço():
     print(' ')
 
-def linha():
-    print('--'*30)
 
+def linha():
+    print('--' * 30)
+
+
+status = True
+status_menu = "opcao"
 valores = list()
 itens = list()
 table = PrettyTable()
-table.field_names = ["No.", " Vlr Digitado", "Celsius", "Kelvin", "Fahrenheit"]
+table.field_names = [" Vlr Digitado", "Celsius", "Kelvin", "Fahrenheit"]
 
-seq = 0
-
-# Verificação de valor digitado
 
 def conversor_add_table(data):
-
     valor = data.get('valor')
     calculos = data.get('calculos')
 
     if valor not in valores:
-        seq = seq=1
         celsius = calculos.get('celsius')
         kelvin = calculos.get('kelvin')
         fahrenheit = calculos.get('fahrenheit')
-
-        table.add_row([seq, valor, celsius, kelvin, fahrenheit])
+        table.add_row([valor, celsius, kelvin, fahrenheit])
         valores.append(valor)
 
-
-
-# Boas-vindas e nome de usuário
-# print('Seja bem-vindo ao nosso conversor de escala termométrica.')
-# nome = str(input('Por favor digite seu nome: '))
-# espaço()
-# print('Seja bem-vindo(a)',nome,'vamos começar.')
-# espaço()
-
+print('''
+             -------------------------------------
+             SISTEMA DE CONVERÇÃO  -   versão 1.0
+             --------------------------------------''')
 while True:
-    valor = float(input('Digite o valor para  converção: '))
+    while status == True:
+        if status_menu == "opcao":
+            print(''' 
+                            MENU
+                         
+                1. Digite um valor para conversão.
+                2. Exibir tabela de resultados.
+                3. Sair do programa
+                ''')
+            escolha = int(input('''
+                Escolha uma das opção e tecle enter: '''))
 
-# Menu inicial
-    escolha = 0
-    while escolha != 6:
+            if escolha == 1:
+                status_menu = "operacao"
+                break
+
+            elif escolha == 2:
+                print(table)
+                break
+
+            elif escolha == 3:
+                status = False
+
+            else:
+                print('Opção errada, tente outra vez.')
+
+        elif status_menu == "operacao":
+
+            valor = float(input('''
+                Digite o valor que você deseja converter? '''))
+
+            print('''      
+                Em qual escala está o valor digitado?              
+                       --------------
+                        1. Celsius
+                        2. Kelvin 
+                        3. Fahrenheit 
+                       --------------''')
+
+            escolha = int(input('''
+                Escolha uma opção e tecle enter: '''))
+
+            if escolha == 1:
+                cel_kel = '{:.2f}'.format(valor + 273.15)
+                cel_fah = '{:.2f}'.format((valor * 9) / 5 + 32)
+                data = {"valor": valor, "calculos": {"kelvin": cel_kel, "fahrenheit": cel_fah},}
+                conversor_add_table(data)
                 print('''
-___________________________________
-            MENU PRINCIPAL
-___________________________________
-  Qual escala voçê quer converter?
-
-1. Celsius
-2. Kelvin 
-3. Fahrenheit 
-4. Nova conversão
-5. Tabela com resultados
-6. Sair deste programa
-____________________________________''')
-                escolha = int(input('Escolha uma opção e tecle enter: '))
-                linha()
+                O valor convertido de:
+                Celsius para Kelvin é = {} K
+                Celsius para Fahrenheit é = {}ºF'''.format(cel_kel, cel_fah))
                 espaço()
-                if escolha == 1:
-                    cel_kel = '{:.1f}'.format(valor + 273.15)
-                    cel_fah = '{:.1f}'.format((valor * 9) / 5 + 32)
-                    data = {"valor": valor,
-                            "calculos": {
-                                "kelvin": cel_kel,
-                                "fahrenheit": cel_fah},
-                            }
-                    conversor_add_table(data)
+                resposta = str(input(''''
+                Deseja continuar? [S/N]'''))
 
-                    print('''O valor convertido de:
-                    Celsius para Kelvin é = {} K
-                    Celsius para Fahrenheit é = {}ºF'''.format(cel_kel, cel_fah))
-                    espaço()
-
-                elif escolha == 2:
-                    kel_cel = '{:.1f}'.format(valor -273.15)
-                    kel_fah = '{:.1f}'.format(((valor - 273.15) / 5) * 9 + 32)
-                    table.add_row([valor, kel_cel, " ", kel_fah])
-                    print('''O valor convertido de:
-                    Kelvin para Celsius é = {} ºC
-                    Kelvin para Fahrenheit é = {} ºF'''.format(kel_cel, kel_fah))
-                    espaço()
-
-                elif escolha == 3:
-                    fah_cel = '{:.1f}'.format((valor - 32) / 9) * 5
-                    fah_kel = '{:.1f}'.format(((valor - 32) / 9) * 5 + 273)
-                    table.add_row([valor, fah_cel, fah_kel, " "])
-                    print('''O valor convertido de:
-                    Fahrenheit para Celsius é = {} ºC
-                    Fahrenheit para Kelvin é = {} K'''.format(fah_cel, fah_kel))
-                    espaço()
-
-                elif escolha == 4:
+                if resposta in 'Ss':
+                    status_menu = 'opcao'
                     break
-                    # valor = float(input('Qual valor você deseja converter? '))
-                    # if valor not in valores:
-                    #     valores.append(valor)
-                    # espaço()
-
-                elif escolha == 5:
-                    print(table)
-
-
-                elif escolha == 6:
-                    print('Finalizando.....')
 
                 else:
-                    print('Opção errada, tente outra vez.')
+                    if resposta in 'Nn':
+                        print(table)
+                    print('''
+                    Fim do programa! Volte sempre!''')
+                    status = False
 
-    print('Fim do programa! Volte sempre!')
-    break
+            elif escolha == 2:
+                kel_cel = '{:.2f}'.format(valor - 273.15)
+                kel_fah = '{:.2f}'.format(((valor - 273.15) / 5) * 9 + 32)
+                data = {"valor": valor, "calculos": {"celsius": kel_cel, "fahrenheit": kel_fah}, }
+                conversor_add_table(data)
+                print('''
+                O valor convertido de:
+                Kelvin para Celsius é = {} ºC
+                Kelvin para Fahrenheit é = {} ºF'''.format(kel_cel, kel_fah))
+                espaço()
+                resposta = str(input('''
+                Deseja continuar? [S/N]'''))
+
+                if resposta in 'Ss':
+                    status_menu = 'opcao'
+                    break
+
+                else:
+                    if resposta in 'Nn':
+                        print(table)
+                    print('''
+                    Fim do programa! Volte sempre!''')
+                    status = False
+
+            elif escolha == 3:
+                fah_cel = '{:.2f}'.format((valor-32) / 1.8)
+                fah_kel = '{:.2f}'.format(((valor - 32) / 9) * 5 + 273)
+                data = {"valor": valor, "calculos": {"celsius": fah_cel, "kelvin": fah_kel}, }
+                conversor_add_table(data)
+                print('''
+                O valor convertido de:
+                Fahrenheit para Celsius é = {} ºC
+                Fahrenheit para Kelvin é = {} K'''.format(fah_cel, fah_kel))
+                espaço()
+                resposta = str(input('''
+                Deseja continuar? [S/N]'''))
+
+                if resposta in 'Ss':
+                    status_menu = 'opcao'
+                    break
+
+                else:
+                    if resposta in 'Nn':
+                        print(table)
+                    print('''
+                    Fim do programa! Volte sempre!''')
+                    status = False
+
+            else:
+                print('Opção errada, tente outra vez.')
+
+    if status == False:
+        break
 
